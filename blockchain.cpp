29215b341de1a96e6bb89c341324bb0d9e6adbaf
Lucas Neves <lucas54neves@gmail.com>
2018-09-26 10:20:26 -0300

    Adding comments in Portuguese to the blockchain.cpp file

--- blockchain.cpp
+++ blockchain.cpp
@@ -3,15 +3,18 @@
 #include "blockchain.hpp"
 #include "block.hpp"
 
+// Construtor da blockchain usando o Bloco Gênesis
 Blockchain::Blockchain() {
     mChain.emplace_back(Block("Bloco gênesis", "0", Size()+1));
 }
 
+// Adiciona um novo bloco à blockchain
 void Blockchain::AddBlock(Block newBlock) {
     newBlock.ModPreviousHash(GetLastBlock().GetHash());
     mChain.push_back(newBlock);
 }
 
+// Valida a blockchain usando as Hash, o tempo de criação e o índice dos blocos
 bool Blockchain::ValidChain() {
     for (unsigned i = 1; i < mChain.size(); ++i) {
         if ((mChain[i-1].GetHash() != mChain[i].GetPreviousHash()) || (mChain[i-1].GetTime() > mChain[i].GetTime()) || (mChain[i-1].GetIndex() > mChain[i].GetIndex())) {
@@ -21,14 +24,17 @@ bool Blockchain::ValidChain() {
     return true;
 }
 
+// Retorna o último bloco da blockchain
 Block Blockchain::GetLastBlock() const {
     return mChain.back();
 }
 
+// Retorna o tamanho da blockchain
 uint32_t Blockchain::Size() {
     return mChain.size();
 }
 
+// Imprime a blockchain a fim de depuração
 void Blockchain::Print() {
     for (uint32_t i = 0; i < Size(); ++i) {
         cout << "Block " << mChain[i].GetIndex() << endl;
